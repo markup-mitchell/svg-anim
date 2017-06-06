4b29ffbaab93774ace3050e807a04c70e3618fd3
spitchell <mr.spitchell@gmail.com>
2017-06-06 18:10:45 +0100

    js script included. animation pending

---
+++ script.js
@@ -0,0 +1,7 @@
+const pod = document.querySelector('#pod');
+const seed = document.querySelector('#seed');
+
+const podLength = pod.getTotalLength();
+const seedLength = seed.getTotalLength();
+
+pod.style.transition = pod.style.WebkitTransition = 'none';
