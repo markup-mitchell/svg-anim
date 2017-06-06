d3b5ab8143ac06c501916cc5df339bab60408b87
spitchell <mr.spitchell@gmail.com>
2017-06-06 21:27:43 +0100

    not working

--- script.js
+++ script.js
@@ -4,4 +4,19 @@ const seed = document.querySelector('#seed');
 const podLength = pod.getTotalLength();
 const seedLength = seed.getTotalLength();
 
+function draw() {
 pod.style.transition = pod.style.WebkitTransition = 'none';
+seed.style.transition = seed.style.WebkitTransition = 'none';
+
+pod.style.strokeDashArray = podLength + ' ' + podLength; 
+seed.style.strokeDashArray = seedLength + ' ' + seedLength; 
+
+pod.style.strokeDashOffset = podLength;
+seed.style.strokeDashOffset = seedLength;
+
+pod.style.transition = pod.style.WebkitTransition = 'stroke-dashoffset 2s ease-in-out';
+seed.style.transition = seed.style.WebkitTransition = 'stroke-dashoffset 2s ease-in-out';
+
+pod.style.strokeDashOffset = '0';
+seed.style.strokeDashOffset = '0';
+}
