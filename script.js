6159349c98a81bfc02237b625780680ed221ce25
spitchell <mr.spitchell@gmail.com>
2017-06-07 07:27:32 +0100

    both transitions working

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 const pod = document.querySelector('#pod');
 const seed = document.querySelector('#seed');
 
+const pod2 = document.querySelector('#pod2');
+const seed2 = document.querySelector('#seed2');
+
 const podLength = pod.getTotalLength();
 const seedLength = seed.getTotalLength();
 
-function draw() {
 pod.style.transition = pod.style.WebkitTransition = 'none';
 seed.style.transition = seed.style.WebkitTransition = 'none';
 
-pod.style.strokeDashArray = podLength + ' ' + podLength; 
-seed.style.strokeDashArray = seedLength + ' ' + seedLength; 
+pod.style.strokeDasharray = podLength + ' ' + podLength; 
+seed.style.strokeDasharray = seedLength + ' ' + seedLength; 
 
-pod.style.strokeDashOffset = podLength;
-seed.style.strokeDashOffset = seedLength;
+pod.style.strokeDashoffset = podLength;
+seed.style.strokeDashoffset = seedLength;
 
-pod.style.transition = pod.style.WebkitTransition = 'stroke-dashoffset 2s ease-in-out';
-seed.style.transition = seed.style.WebkitTransition = 'stroke-dashoffset 2s ease-in-out';
+pod.getBoundingClientRect();
+seed.getBoundingClientRect();
 
-pod.style.strokeDashOffset = '0';
-seed.style.strokeDashOffset = '0';
-}
+pod.style.transition = pod.style.WebkitTransition = 'all 2s ease-in-out';
+// pod.style.transition = pod.style.WebkitTransition = 'stroke-dashoffset 2s ease-in-out';
+// seed.style.transition = seed.style.WebkitTransition = 'stroke-dashoffset 2s ease-in-out';
+pod.style.transition = pod.style.WebkitTransition = 'fill-opacity 2s ease-in-out';
+seed.style.transition = seed.style.WebkitTransition = 'all 7s ease-in-out';
+
+pod2.style.fill = 'linear-gradient(#1FD370, #2988E2)';
+pod.style.fillOpacity = 1;
+seed.style.fillOpacity = 1;
+seed2.style.fill = 'linear-gradient(#FFDB22,#F74E1E)';
+
+pod.style.strokeDashoffset = '0';
+seed.style.strokeDashoffset = '0';
+
+ 
+ pod.style.fill = 'url(#grad1)';
+ seed.style.fill = 'url(#grad2)';
