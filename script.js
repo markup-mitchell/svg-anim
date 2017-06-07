88dffc2cc6ad76bcf4789c67154430adf1906174
spitchell <mr.spitchell@gmail.com>
2017-06-07 16:41:43 +0100

    functions concurrent

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const pod = document.querySelector('#pod');
 const seed = document.querySelector('#seed');
 
-// const pod2 = document.querySelector('#pod2');
-// const seed2 = document.querySelector('#seed2');
-
 const podLength = pod.getTotalLength();
 const seedLength = seed.getTotalLength();
 
@@ -27,14 +24,13 @@ seed.style.transition = seed.style.WebkitTransition = 'stroke-dashoffset 2s ease
 // seed.style.transition = seed.style.WebkitTransition = 'fill-opacity 5s ease-in-out';
 // // seed.style.transition = seed.style.WebkitTransition = 'all 4s ease-in-out';
 
-// pod2.style.fill = 'linear-gradient(#1FD370, #2988E2)';
+function showColor() {
 pod.style.fillOpacity = 1;
 seed.style.fillOpacity = 1;
-// seed2.style.fill = 'linear-gradient(#FFDB22,#F74E1E)';
+};
 
 pod.style.strokeDashoffset = '0';
 seed.style.strokeDashoffset = '0';
-
  
- pod.style.fill = 'url(#grad1)';
- seed.style.fill = 'url(#grad2)';
+pod.style.fill = 'url(#grad1)';
+seed.style.fill = 'url(#grad2)';
