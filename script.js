dca0e73981026029890170ee0a534e2d741bec11
spitchell <mr.spitchell@gmail.com>
2017-06-07 11:47:59 +0100

    transitions both working

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const pod = document.querySelector('#pod');
 const seed = document.querySelector('#seed');
 
-const pod2 = document.querySelector('#pod2');
-const seed2 = document.querySelector('#seed2');
+// const pod2 = document.querySelector('#pod2');
+// const seed2 = document.querySelector('#seed2');
 
 const podLength = pod.getTotalLength();
 const seedLength = seed.getTotalLength();
@@ -19,16 +19,18 @@ seed.style.strokeDashoffset = seedLength;
 pod.getBoundingClientRect();
 seed.getBoundingClientRect();
 
-pod.style.transition = pod.style.WebkitTransition = 'all 2s ease-in-out';
-// pod.style.transition = pod.style.WebkitTransition = 'stroke-dashoffset 2s ease-in-out';
-// seed.style.transition = seed.style.WebkitTransition = 'stroke-dashoffset 2s ease-in-out';
-pod.style.transition = pod.style.WebkitTransition = 'fill-opacity 2s ease-in-out';
-seed.style.transition = seed.style.WebkitTransition = 'all 7s ease-in-out';
+// pod.style.transition = pod.style.WebkitTransition = 'all 2s ease-in-out';
+pod.style.transition = pod.style.WebkitTransition = 'stroke-dashoffset 2s ease-in-out, fill-opacity 5s';
+seed.style.transition = seed.style.WebkitTransition = 'stroke-dashoffset 2s ease-in-out, fill-opacity 5s';
 
-pod2.style.fill = 'linear-gradient(#1FD370, #2988E2)';
+// pod.style.transition = pod.style.WebkitTransition = 'fill-opacity 5s ease-in-out';
+// seed.style.transition = seed.style.WebkitTransition = 'fill-opacity 5s ease-in-out';
+// // seed.style.transition = seed.style.WebkitTransition = 'all 4s ease-in-out';
+
+// pod2.style.fill = 'linear-gradient(#1FD370, #2988E2)';
 pod.style.fillOpacity = 1;
 seed.style.fillOpacity = 1;
-seed2.style.fill = 'linear-gradient(#FFDB22,#F74E1E)';
+// seed2.style.fill = 'linear-gradient(#FFDB22,#F74E1E)';
 
 pod.style.strokeDashoffset = '0';
 seed.style.strokeDashoffset = '0';
