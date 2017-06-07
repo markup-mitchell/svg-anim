464f5479aef79d791326e4c34ed37cfa04acaa90
spitchell <mr.spitchell@gmail.com>
2017-06-07 22:26:20 +0100

    transition chaining added instead of promises

--- script.js
+++ script.js
@@ -17,19 +17,21 @@ pod.getBoundingClientRect();
 seed.getBoundingClientRect();
 
 // pod.style.transition = pod.style.WebkitTransition = 'all 2s ease-in-out';
-pod.style.transition = pod.style.WebkitTransition = 'stroke-dashoffset 2s ease-in-out, fill-opacity 5s';
-seed.style.transition = seed.style.WebkitTransition = 'stroke-dashoffset 2s ease-in-out, fill-opacity 5s';
+pod.style.transition = pod.style.WebkitTransition = 'stroke-dashoffset 1s ease-in-out, fill-opacity 1s 1s, stroke-opacity 2s .5s';
+seed.style.transition = seed.style.WebkitTransition = 'stroke-dashoffset 1s ease-in-out, fill-opacity 1s 1s, stroke-opacity 2s .5s';
 
 // pod.style.transition = pod.style.WebkitTransition = 'fill-opacity 5s ease-in-out';
 // seed.style.transition = seed.style.WebkitTransition = 'fill-opacity 5s ease-in-out';
 // // seed.style.transition = seed.style.WebkitTransition = 'all 4s ease-in-out';
 
-(function showColor() {
 pod.style.fillOpacity = 1;
 seed.style.fillOpacity = 1;
-})();
+
 pod.style.strokeDashoffset = '0';
 seed.style.strokeDashoffset = '0';
+
+pod.style.strokeOpacity = 0;
+seed.style.strokeOpacity = 0;
  
 pod.style.fill = 'url(#grad1)';
 seed.style.fill = 'url(#grad2)';
