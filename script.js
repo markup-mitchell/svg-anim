5bdfc086bc4bf8d78df57bfe128fb6703ca57596
spitchell <mr.spitchell@gmail.com>
2017-07-18 12:56:27 +0100

    outerFace and outerHair working

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ seed.style.strokeDashoffset = seedLength;
 seed.getBoundingClientRect();
 seed.style.transition = seed.style.WebkitTransition = 'stroke-dashoffset 1s ease-in-out';
 seed.style.strokeDashoffset = '0';
+
+const outerHair = document.querySelector('#outerHair');
+const outerHairLength = outerHair.getTotalLength();
+outerHair.style.transition = outerHair.style.WebkitTransition = 'none';
+outerHair.style.strokeDasharray = outerHairLength + ' ' + outerHairLength; 
+outerHair.style.strokeDashoffset = outerHairLength;
+outerHair.getBoundingClientRect();
+outerHair.style.transition = outerHair.style.WebkitTransition = 'stroke-dashoffset 1s ease-in-out';
+outerHair.style.strokeDashoffset = '0';
+
+const outerFace = document.querySelector('#outerFace');
+const outerFaceLength = outerFace.getTotalLength();
+outerFace.style.transition = outerFace.style.WebkitTransition = 'none';
+outerFace.style.strokeDasharray = outerFaceLength + ' ' + outerFaceLength; 
+outerFace.style.strokeDashoffset = outerFaceLength;
+outerFace.getBoundingClientRect();
+outerFace.style.transition = outerFace.style.WebkitTransition = 'stroke-dashoffset 1s ease-in-out';
+outerFace.style.strokeDashoffset = '0';
